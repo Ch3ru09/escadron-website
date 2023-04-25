b5061afd2cb54cd2fd9a57f2adfe1d2d92154d58
Ch3ru09 <80120382+Ch3ru09@users.noreply.github.com>
2023-04-25 12:36:49 -0400

    added background to header

--- src/header/Header.tsx
+++ src/header/Header.tsx
@@ -1,13 +1,16 @@
 import styles from "./Header.module.css";
+import logoSource from "../assets/logo.png";
 
 export default function Header() {
   return (
-    <header>
-      <div class={styles["logo-container"]}>
-        {/* logo */}
+    <header class={`bg-hive ${styles["header"]}`}>
+      <div class={`${styles["logo-container"]}`}>
+        <div class={`${styles["logo"]} not-selectable`}>
+          <img src={logoSource} alt="" />
+        </div>
         <div class={styles["logo-text-container"]}>
-          <div class={styles["logo-text-title"]}>Escadron 811 La Prairie</div>
-          <div class={styles["logo-text-description"]}>Les cadets de l'Aviation royale du Canada</div>
+          <div class={`${styles["logo-text-title"]} not-selectable`}>Escadron 811 La Prairie</div>
+          <div class={`${styles["logo-text-description"]} not-selectable`}>Les cadets de l'Aviation royale du Canada</div>
         </div>
       </div>
       <nav></nav>
