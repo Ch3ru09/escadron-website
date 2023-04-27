e797ba25e56e2cd3ecc16e29b35756814e81e907
Ch3ru09 <80120382+Ch3ru09@users.noreply.github.com>
2023-04-27 10:48:03 -0400

    done header dom

--- src/header/Header.tsx
+++ src/header/Header.tsx
@@ -1,7 +1,10 @@
 import styles from "./Header.module.css";
 import logoSource from "../assets/logo.png";
+import { For } from "solid-js";
 
 export default function Header() {
+  const pages = ["accueil", "inscription", "activités", "cadets", "nous rejoindre"];
+
   return (
     <header class={`bg-hive ${styles["header"]}`}>
       <div class={`${styles["logo-container"]}`}>
@@ -13,14 +16,27 @@ export default function Header() {
           <div class={`${styles["logo-text-description"]} not-selectable`}>Les cadets de l'Aviation royale du Canada</div>
         </div>
       </div>
-      <nav class={styles["navbar"]}>
-        <div class={`${styles["nav-element"]}`}>ACCUEIL</div>
-        <div class={`${styles["nav-element"]}`}>INSCRIPTION</div>
-        <div class={`${styles["nav-element"]}`}>ACTIVITÉS</div>
-        <div class={`${styles["nav-element"]}`}>CADETS</div>
-        <div class={`${styles["nav-element"]}`}>NOUS REJOINDRE</div>
-      </nav>
+      <div class={styles["right-side-container"]}>
+        <p class={styles["cadets-description"]}>Le programme des cadets s'adresse aux jeunes de 12 à 18 ans. Il n'y a aucun frais d'inscription.</p>
+        <nav class={styles["navbar"]}>
+          <For each={pages} fallback={<div>Loading...</div>}>
+            {(name) => <NavElement name={name} />}
+          </For>
+        </nav>
+      </div>
     </header>
   );
 }
+
+function NavElement({ name }: NavProps) {
+  return (
+    <div class={`${styles["nav-element"]}`} tabIndex={0}>
+      {name.toUpperCase()}
+    </div>
+  );
+}
+
+type NavProps = {
+  name: string;
+};
 
