87ff422c00e59c7109bfb70bda5f5da68ff1f421
Ch3ru09 <80120382+Ch3ru09@users.noreply.github.com>
2023-05-02 15:44:44 -0400

    still trying to make it work

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import solid from "solid-start/vite";
 import { defineConfig } from "vite";
+import staticAdapter from "solid-start-static";
 
 export default defineConfig({
-  plugins: [solid()],
+  base: "/my-project/",
+  // insert your github project name between slashes above
+  plugins: [solid({ adapter: staticAdapter() })],
 });
