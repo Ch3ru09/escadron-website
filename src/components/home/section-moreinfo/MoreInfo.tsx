9676f19bb2f44b1a3cba84de95a7ae43d81b4c88
Ch3ru09 <80120382+Ch3ru09@users.noreply.github.com>
2023-08-03 00:10:09 +0800

    remodeled more-info section

--- src/components/home/section-moreinfo/MoreInfo.tsx
+++ src/components/home/section-moreinfo/MoreInfo.tsx
@@ -4,23 +4,27 @@ export default function MoreInfo() {
   return (
     <div class={styles["more-info"]}>
       <h2 class={styles["info-title"]}>ENCOURAGEZ-NOUS, FAITES UN DON QUI FAIT TOUTE LA DIFFÉRENCE</h2>
-      <img src="/logo.png" class={styles["info-logo"]} />
-      <div class={styles["info-donation"]}>
-        <h3>AIDEZ-NOUS DANS NOTRE MISSION AUPRÈS DE LA JEUNESSE LOCALE</h3>
-        <p>
-          L'Escadron 811 La Prairie est un organisme de bienfaisance enregistré auprès de l'Agence du Revenu du Canada.
-          À ce titre, un reçu de don peut être émis pour tout don admissible, lequel peut ensuite être utilisé lors de
-          la production de la déclaration d'impôt sur le revenu du donateur.
-        </p>
-        <button>DÉCOUVREZ COMMENT FAIRE UN DON</button>
-      </div>
-      <div class={styles["info-orgs"]}>
-        <h3>NOS ORGANISMES PARTENAIRES ET BIENFAITEURS</h3>
-        <p>
-          Grâce à la contribution, le soutien et l'appui de ces bienfaiteurs, l'Escadron 811 La Prairie peut réaliser
-          son mandat auprès de la jeunesse, et ce depuis plus de 50 ans.
-        </p>
-        <button>VOIR NOS PARTENAIRES</button>
+      <div class={styles["info-center"]}>
+        <img src="/logo.png" class={styles["info-logo"]} />
+        <div class={styles["info-center-container"]}>
+          <div class={styles["info-donation"]}>
+            <h3>AIDEZ-NOUS DANS NOTRE MISSION AUPRÈS DE LA JEUNESSE LOCALE</h3>
+            <p>
+              L'Escadron 811 La Prairie est un organisme de bienfaisance enregistré auprès de l'Agence du Revenu du
+              Canada. À ce titre, un reçu de don peut être émis pour tout don admissible, lequel peut ensuite être
+              utilisé lors de la production de la déclaration d'impôt sur le revenu du donateur.
+            </p>
+            <button>DÉCOUVREZ COMMENT FAIRE UN DON</button>
+          </div>
+          <div class={styles["info-orgs"]}>
+            <h3>NOS ORGANISMES PARTENAIRES ET BIENFAITEURS</h3>
+            <p>
+              Grâce à la contribution, le soutien et l'appui de ces bienfaiteurs, l'Escadron 811 La Prairie peut
+              réaliser son mandat auprès de la jeunesse, et ce depuis plus de 50 ans.
+            </p>
+            <button>VOIR NOS PARTENAIRES</button>
+          </div>
+        </div>
       </div>
       <div class={styles["info-links"]}>
         <h3>AUTRES LIENS</h3>
@@ -36,7 +40,9 @@ export default function MoreInfo() {
             </a>
           </li>
           <li>
-            <a href="//cadetsair.ca/fr/" target="_blank"></a>
+            <a href="//cadetsair.ca/fr/" target="_blank">
+              La Ligue des cadets de l'air - Québec et Vallée de l'Outaouais
+            </a>
           </li>
         </ul>
       </div>
