2bfb3d35befdf51d890fca6d22dde6c5177f178f
Ch3ru09 <xliang0905@gmail.com>
2023-08-20 22:59:07 -0400

    changed navigation in the header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,10 +1,23 @@
-import { useNavigate } from "solid-start";
+import { A } from "solid-start";
 import styles from "./Header.module.css";
 import { Accessor, For, Setter, createSignal } from "solid-js";
+import { useLocation } from "@solidjs/router";
 
 export default function Header() {
   const pages = ["accueil", "inscription", "activités", "cadets", "nous rejoindre"];
-  const [currentPage, setCurrentPage] = createSignal<string>();
+
+  let pathname = useLocation().pathname.split("/").at(-1);
+
+  if (pathname == undefined) {
+    throw new Error("cannot find path");
+  }
+  console.log(pathname);
+
+  if (pathname == "") {
+    pathname = "/";
+  }
+
+  const [currentPage, setCurrentPage] = createSignal<string>(pathname);
 
   return (
     <header class={`bg-clouds ${styles["header"]}`}>
@@ -14,15 +27,11 @@ export default function Header() {
         </div>
         <div class={styles["logo-text-container"]}>
           <div class={`${styles["logo-text-title"]} not-selectable`}>Escadron 811 La Prairie</div>
-          <div class={`${styles["logo-text-description"]} not-selectable`}>
-            Les cadets de l'Aviation royale du Canada
-          </div>
+          <div class={`${styles["logo-text-description"]} not-selectable`}>Les cadets de l'Aviation royale du Canada</div>
         </div>
       </div>
       <div class={styles["right-side-container"]}>
-        <p class={styles["cadets-description"]}>
-          Le programme des cadets s'adresse aux jeunes de 12 à 18 ans. Il n'y a aucun frais d'inscription.
-        </p>
+        <p class={styles["cadets-description"]}>Le programme des cadets s'adresse aux jeunes de 12 à 18 ans. Il n'y a aucun frais d'inscription.</p>
         <nav class={styles["navbar"]}>
           <For each={pages} fallback={<div>Loading...</div>}>
             {(name) => <NavElement name={name} currentPage={currentPage} setCurrentPage={setCurrentPage} />}
@@ -34,7 +43,6 @@ export default function Header() {
 }
 
 function NavElement({ name, currentPage, setCurrentPage }: NavProps) {
-  const nav = useNavigate();
   const ref: { [key: string]: string } = {
     accueil: "/",
     inscription: "inscription",
@@ -44,25 +52,15 @@ function NavElement({ name, currentPage, setCurrentPage }: NavProps) {
   };
 
   return (
-    <button
-      onclick={(e) => {
-        e.preventDefault();
-        nav(ref[name]);
+    <A
+      onclick={() => {
         setCurrentPage(name);
       }}
-      class={`${styles["nav-element"]} ${currentPage() == name ? styles["current-page"] : ""}`}
-      tabIndex={0}
-    >
+      href={ref[name]}
+      class={`${styles["nav-element"]} ${currentPage() == ref[name] ? styles["current-page"] : ""}`}
+      tabIndex={0}>
       {name}
-    </button>
-
-    // <a
-    //   href={ref[name]}
-    //   class={`${styles["nav-element"]} ${currentPage() == name ? styles["current-page"] : ""}`}
-    //   tabIndex={0}
-    // >
-    //   {name}
-    // </a>
+    </A>
   );
 }
 
@@ -71,4 +69,3 @@ type NavProps = {
   currentPage: Accessor<string | undefined>;
   setCurrentPage: Setter<string>;
 };
-
