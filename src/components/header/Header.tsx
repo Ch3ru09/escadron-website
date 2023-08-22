431f626752da927933248d0b7f19d8e5ecb7f690
Ch3ru09 <xliang0905@gmail.com>
2023-08-21 22:18:59 -0400

    done menu svg

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -26,15 +26,12 @@ export default function Header() {
         </div>
         <div class={styles["logo-text-container"]}>
           <div class={`${styles["logo-text-title"]} not-selectable`}>Escadron 811 La Prairie</div>
-          <div class={`${styles["logo-text-description"]} not-selectable`}>
-            Les cadets de l'Aviation royale du Canada
-          </div>
+          <div class={`${styles["logo-text-description"]} not-selectable`}>Les cadets de l'Aviation royale du Canada</div>
         </div>
+        <Menu />
       </div>
       <div class={styles["right-side-container"]}>
-        <p class={styles["cadets-description"]}>
-          Le programme des cadets s'adresse aux jeunes de 12 à 18 ans. Il n'y a aucun frais d'inscription.
-        </p>
+        <p class={styles["cadets-description"]}>Le programme des cadets s'adresse aux jeunes de 12 à 18 ans. Il n'y a aucun frais d'inscription.</p>
         <nav class={styles["navbar"]}>
           <For each={pages} fallback={<div>Loading...</div>}>
             {(name) => <NavElement name={name} currentPage={currentPage} setCurrentPage={setCurrentPage} />}
@@ -61,10 +58,28 @@ function NavElement({ name, currentPage, setCurrentPage }: NavProps) {
       }}
       href={ref[name]}
       class={`${styles["nav-element"]} ${currentPage() == ref[name] ? styles["current-page"] : ""}`}
-      tabIndex={0}
-    >
+      tabIndex={0}>
       {name}
     </A>
+  );
+}
+
+function Menu() {
+  const [expanded, setExpanded] = createSignal(false);
+
+  return (
+    <button
+      class={styles["menu-button"]}
+      aria-expanded={expanded()}
+      onclick={() => {
+        setExpanded((current) => !current);
+      }}>
+      <svg viewBox="0 0 256 256" stroke="var(--button-color)" fill="none" xmlns="http://www.w3.org/2000/svg" stroke-linecap="round" stroke-linejoin="round">
+        <path class={styles["menu-top"]} d="M26 64 h204.8 v159 h-200 l 200 -200" stroke-width="20" />
+        <path class={styles["menu-middle"]} d="M26 128 h204.8" stroke-width="20" />
+        <path class={styles["menu-bottom"]} d="M26 192 h204.8 v-159 h-200 l200 200" stroke-width="20" />
+      </svg>
+    </button>
   );
 }
 
@@ -73,4 +88,3 @@ type NavProps = {
   currentPage: Accessor<string | undefined>;
   setCurrentPage: Setter<string>;
 };
-
